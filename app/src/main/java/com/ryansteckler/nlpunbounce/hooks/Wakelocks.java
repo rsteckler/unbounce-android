17e29defa92ae86cb109ef6e4a0c5cad691191f6
Ryan Steckler <gotcode@gmail.com>
2014-11-04 11:29:15 -0800

    Bump to 2.0.6

--- app/src/main/java/com/ryansteckler/nlpunbounce/hooks/Wakelocks.java
+++ app/src/main/java/com/ryansteckler/nlpunbounce/hooks/Wakelocks.java
@@ -37,7 +37,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 public class Wakelocks implements IXposedHookLoadPackage {
 
     private static final String TAG = "Amplify: ";
-    public static final String VERSION = "2.0.5"; //This needs to be pulled from the manifest or gradle build.
+    public static final String VERSION = "2.0.6"; //This needs to be pulled from the manifest or gradle build.
     public static final String FILE_VERSION = "3"; //This needs to be pulled from the manifest or gradle build.
     private HashMap<String, Long> mLastWakelockAttempts = null; //The last time each wakelock was allowed.
     private HashMap<String, Long> mLastAlarmAttempts = null; //The last time each alarm was allowed.
